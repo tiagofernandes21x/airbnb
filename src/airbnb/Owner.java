b8b87c3e3473a0102d9db24a7aa46e803e5fb02e
tiagofernandes21x <38888241+tiagofernandes21x@users.noreply.github.com>
2018-12-08 16:23:21 +0000

    criado classe owner

--- src/airbnb/Owner.java
+++ src/airbnb/Owner.java
@@ -10,5 +10,50 @@ package airbnb;
  * @author USER
  */
 public class Owner {
+    private String nome; 
+    private String morada; 
+    private int nif; 
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the morada
+     */
+    public String getMorada() {
+        return morada;
+    }
+
+    /**
+     * @param morada the morada to set
+     */
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+
+    /**
+     * @return the nif
+     */
+    public int getNif() {
+        return nif;
+    }
+
+    /**
+     * @param nif the nif to set
+     */
+    public void setNif(int nif) {
+        this.nif = nif;
+    }
     
 }
